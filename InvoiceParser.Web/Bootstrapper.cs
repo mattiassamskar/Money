4b476a0f7214eb48cfbc0b40e7426ef61a7ea81b
Mattias Sämskar <mattias.samskar@gmail.com>
2017-10-29 12:06:40 +0100

    Add Nancy web host

---
+++ InvoiceParser.Web/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Nancy;
+using Nancy.Conventions;
+using Nancy.TinyIoc;
+
+namespace InvoiceParser.Web
+{
+  public class Bootstrapper:DefaultNancyBootstrapper
+  {
+    protected override void ConfigureConventions(NancyConventions nancyConventions)
+    {
+      base.ConfigureConventions(nancyConventions);
+
+      nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory(@"/", @"/Content"));
+    }
+
+    protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+    {
+      base.ConfigureApplicationContainer(container);
+
+      container.Register<IMediator>((x, overloads) => new Mediator(x.Resolve, x.ResolveAll));
+    }
+  }
+}
---
+++ InvoiceParser.Web/ContentModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace InvoiceParser.Web
+{
+  public class ContentModule : NancyModule
+  {
+    public ContentModule()
+    {
+      Get["/"] = _ => Response.AsFile("Content/index.html");
+    }
+  }
+}
