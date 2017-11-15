c5e43121d843c26c392ffaf480f325c02dbdcbf4
Mattias Sämskar <mattias.samskar@gmail.com>
2017-11-15 12:18:48 +0100

    Add chart.js

--- Money.Web/Content/index.js
+++ Money.Web/Content/index.js
@@ -1,33 +1,38 @@
 var app = new Vue({
     el: '#app',
-    mounted: () => {
-        setTimeout(() => {
+    mounted: function () {
+        var self = this;
+        setTimeout(function () {
             var connection = $.hubConnection();
             var proxy = connection.createHubProxy('expenseHub');
-            proxy.on('send', (expenses) => {
-                app.expenses.push(...expenses);
+            proxy.on('send', function(expenses) {
+                self.expenses.push(...expenses);
             });
             connection.start();
         }, 1000);
+        var ctx = document.getElementById("chart").getContext('2d');
+        this.chart = new Chart(ctx, { type: 'bar' });
     },
     data: {
         filter: '',
         expenses: [],
-        sum: 0
+        sum: 0,
+        chart: ''
     },
     methods: {
-        filtered: (expenses, filterString) =>
-            expenses.filter((expense) =>
-                filterString.split(',').filter((f) =>
-                    expense.Description.includes(f)).length > 0)
-        ,
-        totalSum: (expenses, filterString) => {
-            const amounts = expenses.filter((expense) =>
-                filterString.split(',').filter((f) =>
-                    expense.Description.includes(f)).length > 0).map(expense => expense.Amount);
-            return amounts.reduce((a, b) => a + b, 0);
+        filtered: function () {
+            var self = this;
+            var hej = self.expenses.filter(function (expense) {
+                return self.filter.split(',').filter(function (f) {
+                    return expense.Description.includes(f)
+                }).length > 0
+            });
+            return hej;
         },
-        drop: (ev) => {
+        totalSum: function () {
+            return this.filtered().reduce((a, b) => a + b.Amount, 0);
+        },
+        drop: function (ev) {
             ev.preventDefault();
             var dt = ev.dataTransfer;
             for (var i = 0; i < dt.files.length; i++) {
@@ -38,7 +43,17 @@ var app = new Vue({
                 xhr.send(fd);
             }
         },
-        dragover: (ev) => ev.preventDefault(),
-        dragEnd: (ev) => ev.dataTransfer.clearData(),
+        dragover: function (ev) { ev.preventDefault() },
+        dragEnd: function (ev) { ev.dataTransfer.clearData() },
+        updateChart: function (ev) {
+            this.chart.data.labels.pop();
+            this.chart.data.datasets = [];
+            var labels = this.filtered().map(function(e) {return e.Date});
+            var data = this.filtered().map(function (e) { return e.Amount });
+
+            this.chart.data.labels.push(labels);
+            this.chart.data.datasets.push({data: data });
+            this.chart.update();
+        }
     }
 })
