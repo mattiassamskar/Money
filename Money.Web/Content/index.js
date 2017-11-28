3d48e6337396ceda53e0011c85d37cfc3cf60f84
Mattias Sämskar <mattias.samskar@gmail.com>
2017-11-28 19:36:00 +0100

    Replace signalR with rest call in client. Add spinner to indicate pending request. Use cloudflare cdn for libraries. Configure chart to begin at 0.

--- Money.Web/Content/index.js
+++ Money.Web/Content/index.js
@@ -1,57 +1,71 @@
 var app = new Vue({
-    el: '#app',
-    mounted: function () {
-        var self = this;
-        var connection = $.hubConnection();
-        var proxy = connection.createHubProxy('expenseHub');
-        proxy.on('send', function(expenses) {
-            self.expenses.push(...expenses);
-        });
-        connection.start();
-        var ctx = document.getElementById("chart").getContext('2d');
-        this.chart = new Chart(ctx, { type: 'line', options: { legend: { display: false } } });
-    },
-    data: {
-        filter: '',
-        expenses: [],
-        sum: 0,
-        chart: ''
-    },
-    methods: {
-        filtered: function () {
-            var self = this;
-            return self.expenses.filter(function (expense) {
-                return self.filter.split(',').filter(function (f) {
-                    return expense.Description.includes(f)
-                }).length > 0
-            });
+  el: '#app',
+  mounted: function () {
+    var ctx = document.getElementById("chart").getContext('2d');
+    this.chart = new Chart(ctx, {
+      type: 'line',
+      options: {
+        legend: {
+          display: false
         },
-        totalSum: function () {
-            return this.filtered().reduce((a, b) => a + b.Amount, 0);
+        elements: {
+          line: {
+            tension: 0
+          }
         },
-        drop: function (ev) {
-            ev.preventDefault();
-            var dt = ev.dataTransfer;
-            for (var i = 0; i < dt.files.length; i++) {
-                var xhr = new XMLHttpRequest();
-                xhr.open("POST", 'upload', true);
-                var fd = new FormData();
-                fd.append("upload_file", dt.files[i]);
-                xhr.send(fd);
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
             }
-        },
-        dragover: function (ev) { ev.preventDefault() },
-        dragEnd: function (ev) { ev.dataTransfer.clearData() },
-        updateChart: function (ev) {
-            var result = this.filtered().reduce((a, b) => {
-                a[b.Date.substring(0, 7)] = a[b.Date.substring(0, 7)] || 0;
-                a[b.Date.substring(0, 7)] = a[b.Date.substring(0, 7)] + b.Amount;
-                return a;
-            }, []);
-
-            this.chart.data.labels = [...Object.keys(result)];
-            this.chart.data.datasets = [{ data: [...Object.values(result)] }];
-            this.chart.update();
+          }]
         }
+      }
+    });
+  },
+  data: {
+    filter: '',
+    expenses: [],
+    sum: 0,
+    chart: '',
+    hideSpinner: true
+  },
+  methods: {
+    totalSum: function () {
+      return this.expenses.reduce((a, b) => a + b.amount, 0);
+    },
+    drop: function (ev) {
+      ev.preventDefault();
+      var dt = ev.dataTransfer;
+      for (var i = 0; i < dt.files.length; i++) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", 'upload', true);
+        var fd = new FormData();
+        fd.append("upload_file", dt.files[i]);
+        xhr.send(fd);
+      }
+    },
+    dragover: function (ev) { ev.preventDefault(); },
+    dragEnd: function (ev) { ev.dataTransfer.clearData(); },
+    updateChart: function (ev) {
+      var self = this;
+      self.hideSpinner = false;
+      var req = new XMLHttpRequest();
+      req.onload = function () {
+        self.expenses = (JSON.parse(req.response));
+        var result = self.expenses.reduce((a, b) => {
+          var month = new Date(b.date).toLocaleDateString('sv-SE').substring(0, 7);
+          a[month] = a[month] || 0;
+          a[month] = a[month] + b.amount;
+          return a;
+        }, []);
+        self.chart.data.labels = [...Object.keys(result)];
+        self.chart.data.datasets = [{ data: [...Object.values(result)] }];
+        self.chart.update();
+        self.hideSpinner = true;
+      };
+      req.open('GET', '/expenses' + '?filter=' + self.filter);
+      req.send();
     }
-})
+  }
+});
