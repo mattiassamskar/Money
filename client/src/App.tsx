9e6acd3640790762d6aff141a251c940b6135613
Mattias Sämskar <mattias.samskar@gmail.com>
2019-02-02 15:20:32 +0100

    Add expenses container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import "antd/dist/antd.css";
+import { Row, Col } from "antd";
+import ExpensesContainer from "./ExpensesContainer";
 
 export interface Expense {
   id: string;
@@ -10,9 +12,35 @@ export interface Expense {
 }
 
 class App extends Component {
+  expenses: Expense[] = [
+    {
+      id: "1",
+      date: "2019-01-01",
+      description: "ICA",
+      amount: 2500
+    },
+    {
+      id: "2",
+      date: "2019-03-01",
+      description: "Coop",
+      amount: 990
+    },
+    {
+      id: "3",
+      date: "2019-02-01",
+      description: "Stadium",
+      amount: 35
+    }
+  ];
+
   render() {
     return (
       <div className="App">
+        <Row>
+          <Col span={24}>
+            <ExpensesContainer expenses={this.expenses} />
+          </Col>
+        </Row>
       </div>
     );
   }
--- client/src/ExpensesContainer.tsx
+++ client/src/ExpensesContainer.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Expense } from "./App";
+import Table from "antd/lib/table";
 
 interface Props {
   expenses: Expense[];
@@ -8,8 +9,26 @@ interface Props {
 interface State {}
 
 class ExpensesContainer extends Component<Props, State> {
+  columns = [
+    {
+      title: "Datum",
+      dataIndex: "date",
+      key: "date"
+    },
+    {
+      title: "Beskrivning",
+      dataIndex: "description",
+      key: "description"
+    },
+    {
+      title: "Belopp",
+      dataIndex: "amount",
+      key: "amount"
+    }
+  ];
+
   render() {
-    return <div className="App" />;
+    return <Table dataSource={this.props.expenses} columns={this.columns} />;
   }
 }
 
