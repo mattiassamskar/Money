8a1e905b7e8d2baa03ff48c5108ea9279284604f
Mattias Sämskar <mattias.samskar@gmail.com>
2019-03-16 08:42:40 +0100

    Move styling

--- client/src/HeaderComponent.tsx
+++ client/src/HeaderComponent.tsx
@@ -2,26 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Icon } from "antd";
 import "antd/dist/antd.css";
+import "./header.css";
 
 export const Header = () => (
-  <div
-    style={{
-      height: "60px",
-      paddingLeft: "10px",
-      paddingRight: "10px",
-      marginBottom: "10px",
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
-      background: "#fafafa"
-    }}
-  >
-    <Link to="/" style={{ color: "black" }}>
+  <div className="header-container">
+    <Link to="/" className="header-homelink">
       <div>
         <Icon type="home" /> Ekonomi
       </div>
     </Link>
-    <Link to="/edit" style={{ color: "grey" }}>
+    <Link to="/edit" className="header-editlink">
       Ändra
     </Link>
   </div>
