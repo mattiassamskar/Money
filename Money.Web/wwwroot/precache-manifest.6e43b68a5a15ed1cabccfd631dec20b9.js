962ea0ca06a51d59a208bc8db2b9ceddc96c35db
Mattias Sämskar <mattias.samskar@gmail.com>
2019-07-18 09:53:59 +0200

    Update client

---
+++ Money.Web/wwwroot/precache-manifest.6e43b68a5a15ed1cabccfd631dec20b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12218010a718a4347f34",
+    "url": "/static/css/main.b5c38b52.chunk.css"
+  },
+  {
+    "revision": "12218010a718a4347f34",
+    "url": "/static/js/main.376035a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "be6492ce2b3778773156",
+    "url": "/static/css/2.022f575b.chunk.css"
+  },
+  {
+    "revision": "be6492ce2b3778773156",
+    "url": "/static/js/2.1aece4fd.chunk.js"
+  },
+  {
+    "revision": "8589c103ead604581260eae2754916f8",
+    "url": "/index.html"
+  }
+];
--- Money.Web/wwwroot/service-worker.js
+++ Money.Web/wwwroot/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e515c67684d755444b47418edfc2befa.js"
+  "/precache-manifest.6e43b68a5a15ed1cabccfd631dec20b9.js"
 );
 
 workbox.clientsClaim();
