350c84509bab097e523bfbc01989ecdfb185d90a
Mattias Sämskar <mattias.samskar@gmail.com>
2025-12-14 13:24:22 +0100

    chore: improve local dev setup (#47)

---
+++ Money.Db/MockDbService.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Money.Core;
+using Money.Core.Models;
+
+namespace Money.Db
+{
+  public class MockDbService : IDbService
+  {
+    private readonly List<Expense> _expenses;
+    private readonly List<Filter> _filters;
+
+    public MockDbService()
+    {
+      _expenses = new List<Expense>();
+      _filters = new List<Filter>();
+    }
+
+    public ICollection<Expense> GetExpenses()
+    {
+      return _expenses;
+    }
+
+    public ICollection<Expense> GetFilteredExpenses(IEnumerable<string> filters, string month)
+    {
+      return filters.Any() ? filters.SelectMany(filter => _expenses.FindAll(expense => expense.Description.ToLower().Contains(filter.ToLower()))).ToList() : GetExpenses();
+    }
+
+    public void AddExpenses(IEnumerable<Expense> expenses)
+    {
+      _expenses.AddRange(expenses);
+    }
+
+    public void AddExpense(Expense expense)
+    {
+      _expenses.Add(expense);
+    }
+
+    public void DeleteExpense(string objectId)
+    {
+      _expenses.Remove(_expenses.Single(expense => expense.Id == objectId));
+    }
+
+    public ICollection<Filter> GetFilters()
+    {
+      return _filters;
+    }
+
+    public void AddFilter(Filter filter)
+    {
+      _filters.Add(filter);
+    }
+
+    public void DeleteFilter(string objectId)
+    {
+      _filters.Remove(_filters.Single(filter => filter.Id == objectId));
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Money
+
 [![Build and deploy](https://github.com/mattiassamskar/Money/actions/workflows/master_money-app.yml/badge.svg)](https://github.com/mattiassamskar/Money/actions/workflows/master_money-app.yml)
+
+## Develop
+
+### Backend
+
+`dotnet run` in Money.Web project starts the API on port 5000.
+
+`MockDbService` can be used as in-memory mock database.
+
+Configure `MoneyDbConnectionString` environment variable to use prod database.
+
+### Frontend
+
+`npm run dev` in client folder starts dev server.
